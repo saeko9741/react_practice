0a1d6606cfb31612809a746f08f483fb01d43364
saeko <58896213+saeko9741@users.noreply.github.com>
2021-01-01 23:25:43 +0900

    Add component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    // </React.Fragment>短縮して書ける
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 // htmlにコンポーネントを反映するため
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => {
-  return (
-    // </React.Fragment>短縮して書ける
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
 // 画面に反映させる render関数の第一引数にコンポーネント、第二引数はどこに反映させるか、
 ReactDom.render(<App />, document.getElementById("root"));
